d1716cec9e4e3f0341cce291d37a7f827c86cdda
Peter Deng <peter.deng@savills.com>
2024-12-18 15:51:28 +0000

    test

--- nfpwbase-main/tests/example.spec.ts
+++ nfpwbase-main/tests/example.spec.ts
@@ -1,6 +1,7 @@
-import {test, expect} from '@playwright/test'
+import {test, expect} from '@playwright/test';
 
 test("Open Google", async ({page}) => {
-    await page.goto("https://www.google.com")
-    await expect(page).toHaveTitle("Google")
-})
+    await page.pause();
+    await page.goto("https://www.google.com");
+    await expect(page).toHaveTitle("Google");
+});
